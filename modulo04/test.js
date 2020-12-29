47ba86c1b4dfa6a030a47ae201086491bae50e0a
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-12-29 14:30:00 -0300

    07 - manipulando arquivos - create

--- modulo04/test.js
+++ modulo04/test.js
@@ -9,15 +9,23 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suite de manipulação de heróis', () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+  })
+
   it('pesquisar um heroi usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
+
+
     const [resultado] = await database.listar(expected.id)
 
     deepStrictEqual(resultado, expected)
   })
-  // it('Deve cadastrar herói, usando arquivos', async () => {
-  //   const expected = DEFAULT_ITEM_CADASTRAR
-  //   //
-  //   ok(null, expected)
-  // })
+  it('Deve cadastrar herói, usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+
+    deepStrictEqual(actual, expected)
+  })
 })
