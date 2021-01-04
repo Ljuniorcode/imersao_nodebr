2a5b3efbe17c922321cabc59279afbdcc98024ab
Ljuniorcode <lucianojuniorarl@gmail.com>
2021-01-03 23:13:27 -0300

    18 - up e del

--- multi-db-postgres/src/tests/postgresStrategy.test.js
+++ multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -5,11 +5,16 @@ const Context = require('../db/strategies/base/contextStrategy')
 const context = new Context(new Postgres())
 const MOCK_HEROI_CADASTRAR = { nome: 'Gaviao Negro', poder: 'flexas' }
 
+const MOCK_HEROI_ATUALIZAR = { nome: 'Batman', poder: 'dinheiro' }
+
+
 
 describe('Postgres Strategy', function () {
   this.timeout(Infinity)
   this.beforeAll(async function () {
     await context.connect()
+    await context.delete()
+    await context.create(MOCK_HEROI_ATUALIZAR)
   })
   it('PostgresSql Connection', async function () {
     const result = await context.isConnected()
@@ -28,7 +33,28 @@ describe('Postgres Strategy', function () {
     // const posicaoZero = result[0]
     // const [posicao1, posicao2] = ['primeiro'],'segundo']
 
-
     assert.strictEqual(result, MOCK_HEROI_CADASTRAR)
+  })
+
+  it('atualizar', function () {
+    const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_ATUALIZAR.nome })
+    const novoItem = {
+      ...MOCK_HEROI_ATUALIZAR, //pega tudo
+      nome: 'Mulher maravilha' //atualiza somente o campo nome
+    }
+    /*
+    No javascript tem uma técnica chamada rest/spred
+    usado para mergear objetos ou separá-lo
+    */
+    const [result] = await context.update(itemAtualizar.id, novoItem)
+    const [itemAtualizado] = await context.read({ id: itemAtualizar.id })
+
+    assert.strictEqual(result, 1)
+    assert.strictEqual(itemAtualizado.nome, novoItem.nome)
+  })
+  it('remover por id', async function () {
+    const [item] = await context.read({})
+    const result = await context.delete(item.id)
+    assert.strictEqual(result, 1)
   })
 })
